201b0985684968818a96c2987e131a36526f124c
Alina <alina09091999@yandex.ru>
2024-01-09 00:28:56 +0300

    changed the names of tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 
 public class CashbackHackServiceTest {
     @Test
-    public void test() {
+    public void shouldAmountIs150() {
         CashbackHackService service = new CashbackHackService();
         int amount = 150;
         int actual = service.remain(amount);
@@ -16,7 +16,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void test5() {
+    public void shouldAmountIs300() {
         CashbackHackService service = new CashbackHackService();
         int amount = 300;
         int actual = service.remain(amount);
@@ -27,7 +27,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void test1() {
+    public void shouldAmountIs1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
@@ -38,7 +38,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void test2() {
+    public void shouldAmountIs0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
